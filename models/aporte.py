e6f5bb57175047e10b33e20baedd67021e501604
QwaserTi <rnstljndr@gmail.com>
2026-06-30 18:34:19 -0600

    Actualizar aporte.py

--- models/aporte.py
+++ models/aporte.py
@@ -5,14 +5,29 @@ import uuid
 class Aporte:
 
     def __init__(self):
+        # Identificador único del aporte
         self.id = str(uuid.uuid4())
 
+        # Estado del flujo
         self.estado = "WAITING_COMMENT"
+
+        # Comentario del usuario
         self.comentario = ""
 
-        self.archivos = []
-        self.albums = {}
+        # Línea de tiempo del aporte.
+        # Aquí se guardará TODO respetando el orden:
+        #
+        # [
+        #     {"tipo": "album", "media_group_id": "...", "items": [...]},
+        #     {"tipo": "video", "file_id": "..."},
+        #     {"tipo": "photo", "file_id": "..."},
+        #     {"tipo": "document", "file_id": "..."}
+        # ]
+        #
+        self.timeline = []
 
+        # Fecha de creación
         self.created_at = datetime.now()
 
+        # Mensaje del panel "Aporte en progreso"
         self.status_message_id = None
