970312a924cc96cbe6f8a8abd2e8f00d0cef13e2
QwaserTi <rnstljndr@gmail.com>
2026-07-01 16:57:47 -0600

    Actualizar start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,18 +1,18 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
-
-from services.aporte_manager import manager
 
 
 def menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("🆕 Nuevo aporte", callback_data="nuevo_aporte")]
-    ])
+    return InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🆕 Nuevo aporte", callback_data="nuevo_aporte")]]
+    )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message:
+        return
 
     await update.message.reply_text(
         "💌 Bienvenido a KikuAportes 💌\n\nPulsa para iniciar un aporte:",
-        reply_markup=menu()
+        reply_markup=menu(),
     )
